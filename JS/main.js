8ad5d6b3b3dded24f2176a9fbb304af407a9be2c
Iván Calcagno <ivancalcagno3003@gmail.com>
2023-07-05 22:10:33 -0300

    Subiendo Página de home con header y footer

--- JS/main.js
+++ JS/main.js
@@ -8,25 +8,85 @@ btnNav.addEventListener("click", () => {
 });
 
 let main = document.querySelector("#main");
+let mainCenter = document.querySelector("#main-center");
 let links = document.querySelectorAll(".links");
 for (let link of links) {
     link.addEventListener("click", (e) => {
         e.preventDefault();
         let id = link.dataset.link;
         console.log(id);
+        loadContent(id);
         switch (id) {
             case "home":
-                loadHome();
+                console.log("home");
+                break;
+            case "catalogo":
+                console.log("catalogo");
                 break;
         }
     });
 }
 
-document.addEventListener("DOMContentLoaded", loadHome);
-async function loadHome() {
-    let responseHome = await fetch("../HTML/home.html");
+document.addEventListener("DOMContentLoaded", () => {
+    loadContent("home");
+});
+
+async function loadContent(id) {
+    loading(1);
+    let responseHome = await fetch(`../HTML/${id}.html`);
     if (responseHome.ok) {
         let homeHtml = await responseHome.text();
-        console.log(homeHtml);
+        mainCenter.innerHTML = homeHtml;
+    }
+}
+
+function loading(main) {
+    switch (main) {
+        case 1:
+            mainCenter.innerHTML = `
+                <h5 class="card-title placeholder-glow">
+                    <span class="placeholder col-6"></span>
+                </h5>
+            <p class="card-text placeholder-glow">
+                <span class="placeholder col-7"></span>
+                <span class="placeholder col-4"></span>
+                <span class="placeholder col-4"></span>
+                <span class="placeholder col-6"></span>
+                <span class="placeholder col-8"></span>
+            </p>
+            <h5 class="card-title placeholder-glow">
+                    <span class="placeholder col-6"></span>
+                </h5>
+            <p class="card-text placeholder-glow">
+                <span class="placeholder col-7"></span>
+                <span class="placeholder col-4"></span>
+                <span class="placeholder col-4"></span>
+                <span class="placeholder col-6"></span>
+                <span class="placeholder col-8"></span>
+                </p>`;
+            break;
+        case 2:
+            main.innerHTML = `
+                    <h5 class="card-title placeholder-glow">
+                        <span class="placeholder col-6"></span>
+                    </h5>
+                <p class="card-text placeholder-glow">
+                    <span class="placeholder col-7"></span>
+                    <span class="placeholder col-4"></span>
+                    <span class="placeholder col-4"></span>
+                    <span class="placeholder col-6"></span>
+                    <span class="placeholder col-8"></span>
+                </p>
+                <h5 class="card-title placeholder-glow">
+                        <span class="placeholder col-6"></span>
+                    </h5>
+                <p class="card-text placeholder-glow">
+                    <span class="placeholder col-7"></span>
+                    <span class="placeholder col-4"></span>
+                    <span class="placeholder col-4"></span>
+                    <span class="placeholder col-6"></span>
+                    <span class="placeholder col-8"></span>
+                </p>`;
+            break;
     }
 }
